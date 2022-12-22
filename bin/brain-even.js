742c6d7d8fe48734ddc210dec68dc21414d01420
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2022-12-22 16:39:44 +0700

    add brain-calc.js and fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
 
-const getRandomNumber = () => Math.floor(Math.random() * 50);
+import startEvenGame from '../src/games/gameEven.js';
 
-const startEvenGame = () => {
-  console.log('Welcome to the Brain Games!');
-
-  const userName = readlineSync.question('May I have your name? ');
-
-  console.log(`Hello, ${userName}!`);
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const roundCount = 3;
-
-  for (let i = 0; i < roundCount; i += 1) {
-    const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
-    const answer = readlineSync.question('Your answer: ');
-
-    if (isEven === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
 startEvenGame();
