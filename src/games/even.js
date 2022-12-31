bb3de1ef4d2555ff4ef4ddc7a788b54de91737e4
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2022-12-31 16:04:25 +0700

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -9,10 +9,10 @@ const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const number = getRandomNumber(minNumber, maxNumber);
-  const answer = isEven(number) ? 'yes' : 'no';
+  const expectedAnswer = isEven(number) ? 'yes' : 'no';
   const question = String(number);
 
-  return [question, answer];
+  return [question, expectedAnswer];
 };
 
 const startEven = () => {
